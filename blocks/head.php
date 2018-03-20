49dd15592b3af6c8b347943d33543d288fb1f71a
Alexander <36115344+Slesarson@users.noreply.github.com>
2018-03-20 13:59:12 +0300

    фыв

--- blocks/head.php
+++
@@ -1,8 +0,0 @@
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<title> <?=$title?> </title>
-	<link rel="stylesheet" href="css/bootstrap.css" >
-	<link rel="stylesheet" href="../css/styles.css">
-	<!--[if lt IE 9]>
-	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
